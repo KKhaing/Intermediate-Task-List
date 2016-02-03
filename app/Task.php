a042e70aad4bd72e9e0650366103f2ec758ac059
shwezin <zadeveloper02@gmail.com>
2016-02-03 11:05:34 +0630

    database

---
+++ app/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Task extends Eloquent 
+{
+	protected $collection = 'tasks';
+
+	protected $fillable = ['name'];
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+}
